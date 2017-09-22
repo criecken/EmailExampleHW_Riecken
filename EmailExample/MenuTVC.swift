50bc8d738b6b9e69261f3547c0b363ae4adb2b12
ABA Lab <criecken@asu.edu>
2017-09-22 14:47:33 -0700

    Added recipient, plans to initiate added buttons; TODO: Add recipients to emails, labels to buttons

--- EmailExample/MenuTVC.swift
+++ EmailExample/MenuTVC.swift
@@ -20,7 +20,7 @@ class MenuTVC: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
